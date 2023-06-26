6c33848901d5d8037b8816a97282ca3eeb35863a
Drew Cavanaugh <drewcav96@gmail.com>
2023-06-26 15:47:34 -0400

    Add project files.

---
+++ Portfolio/Pages/Index.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace Portfolio.Pages
+{
+    public partial class Index
+    {
+        private static readonly DateTime BirthDate = new(1996, 12, 3);
+        private int _age;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            _age = (DateTime.Today - BirthDate).Days / 365;
+        }
+    }
+}
