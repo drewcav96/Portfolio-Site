cb89ac98b3d040edd75df3e512e7127fba8e73ae
Drew Cavanaugh <drewcav96@gmail.com>
2023-07-10 19:10:40 -0400

    Add Lightbox

---
+++ Portfolio/Pages/Projects/Invoice.razor.cs
@@ -0,0 +1,30 @@
+﻿using Blazorise;
+using Microsoft.AspNetCore.Components;
+using Portfolio.Pages.Shared;
+
+namespace Portfolio.Pages.Projects
+{
+    public partial class Invoice
+    {
+        private Tabs _tabsShowcase = null!;
+
+        private Task ShowLightboxOnClick()
+        {
+            return _tabsShowcase.SelectedTab switch
+            {
+                "field" => ModalService.Show<Lightbox>(p => p.Add(c => c.ChildContent, _cslField)),
+                "ap" => ModalService.Show<Lightbox>(p => p.Add(c => c.ChildContent, _cslAP)),
+                "audit" => ModalService.Show<Lightbox>(p => p.Add(c => c.ChildContent, _cslAudit)),
+                "sys" => ModalService.Show<Lightbox>(p => p.Add(c => c.ChildContent, _cslSys)),
+                _ => Task.CompletedTask
+            };
+        }
+
+        #region Parameters
+
+        [Inject]
+        protected IModalService ModalService { get; private set; } = default!;
+
+        #endregion
+    }
+}
---
+++ Portfolio/Pages/Shared/Lightbox.razor.cs
@@ -0,0 +1,23 @@
+﻿using Blazorise;
+using Microsoft.AspNetCore.Components;
+
+namespace Portfolio.Pages.Shared
+{
+    public partial class Lightbox
+    {
+        private Task CloseLightboxOnClick()
+        {
+            return ModalService.Hide();
+        }
+
+        #region Parameters
+
+        [Inject]
+        protected IModalService ModalService { get; private set; } = default!;
+
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        #endregion
+    }
+}
